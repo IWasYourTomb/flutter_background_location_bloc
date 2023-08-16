e41d2d3949e850472f0d9fe6a13a9c11789e8d0a
IWasYourTomb <67316822+IWasYourTomb@users.noreply.github.com>
2023-08-16 15:12:04 +0300

    Changes

---
+++ lib/internal/app.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:geotest/domain/bloc/location_bloc/location_bloc.dart';
+import 'package:geotest/presentation/screens/home/home_screen.dart';
+import 'package:geotest/untils/bloc/bloc_inital.dart';
+import 'package:flutter/material.dart';
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: BlocInitial.bloc,
+      child: BlocBuilder<LocationBloc, LocationState>(
+        builder: (context, state) {
+          return const MaterialApp(
+              debugShowCheckedModeBanner: false, home: HomePage());
+        },
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:geotest/domain/bloc/location_bloc/location_bloc.dart';
-import 'package:geotest/presentation/screens/home/home_screen.dart';
-import 'package:geotest/untils/bloc/bloc_inital.dart';
+import 'package:geotest/internal/app.dart';
 import 'internal/di/inject.dart';
 
 void main() {
@@ -10,19 +7,3 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: BlocInitial.bloc,
-      child: BlocBuilder<LocationBloc, LocationState>(
-        builder: (context, state) {
-          return const MaterialApp(
-              debugShowCheckedModeBanner: false, home: HomePage());
-        },
-      ),
-    );
-  }
-}
